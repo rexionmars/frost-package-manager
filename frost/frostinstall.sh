46949b35cc2e6a3d4e24e0fab144d892f4cb6e96
rexionmars <opensource.leonardi@gmail.com>
2023-06-10 22:02:41 -0300

    Improved code

--- frost/frostinstall.sh
+++ frost/frostinstall.sh
@@ -1,38 +1,33 @@
 #!/usr/bin/env sh 
 
-#    ______ _____   ____   _____ _______ _____ _   _  _____ _______       _      _      
-#   |  ____|  __ \ / __ \ / ____|__   __|_   _| \ | |/ ____|__   __|/\   | |    | |     
-#   | |__  | |__) | |  | | (___    | |    | | |  \| | (___    | |  /  \  | |    | |     
-#   |  __| |  _  /| |  | |\___ \   | |    | | | . ` |\___ \   | | / /\ \ | |    | |     
-#   | |    | | \ \| |__| |____) |  | |   _| |_| |\  |____) |  | |/ ____ \| |____| |____ 
-#   |_|    |_|  \_\\____/|_____/   |_|  |_____|_| \_|_____/   |_/_/    \_\______|______|
-#
+########################################
+# Author: Leonardi Melo                #
+# Email: opensource.leonardi@gmail.com #
+# Github: rexionmars                   #
+########################################
 
-### Config Package Tracker
+# Package Tracker location folder
 TRACK='/var/log/frost'
 
-### Creating the tracker directory
+# Creating the tracker directory
 [ ! -d "$TRACK" ] && mkdir -v "$TRACK"
 
 # Function for install package
 INSTALL()
 {
-  # Get user package name
   local package="$1"
 
   printf '%b' "initialize package instalation: $package\n"
-
   tar xvf "$package" -C /ssf/virtual
-
   return 0
 }
 
-### User check params
+# User check params
 case $1 in
-  -i|--install)
+  --install|-i)
       shift
       if [ -e "$1" ]; then
-        INSTALL $1 # Call install function
+        INSTALL $1
       else
         echo "Package $1 is not exitent"
         exit 1
