163e07c0515494d7a47052531df0e0e3dbde5a99
rexionmars <opensource.leonardi@gmail.com>
2023-07-02 17:20:09 -0300

    [feat] Implemented install mode and clean mode from the package install

--- frost/frostinstall.sh
+++ frost/frostinstall.sh
@@ -7,19 +7,35 @@
 ########################################
 
 # Package Tracker location folder
-TRACK='/var/log/frost'
+TRACK='/var/log/ices'
 
-# Creating the tracker directory
+# Creating the tracker directory if not exists
 [ ! -d "$TRACK" ] && mkdir -v "$TRACK"
 
 # Function for install package
 INSTALL()
 {
   local package="$1"
+  package_name=$(basename $package)     # get only package name
+  package_name="${package_name//.ice/}" # drop exetension
+  echo "$package_name"
 
+  # Unpack the package in system
   printf '%b' "initialize package instalation: $package\n"
-  tar xvf "$package" -C /ssf/virtual
-  return 0
+  if tar xvf "$package" -C / >> "${TRACK}/${name_package.track}"; then
+    echo "The package ${packa_name} as bieen installed"
+  else
+    echo "The package ${package_name} not installed"
+    exit 1
+  fi
+
+  # Remove ./ and blank lines
+  if sed -i 's,\.\/,,g; /^$/d' "${TRACK}/${package_name}.track"; then
+    echo "[INFO] Cleaning is runnig"
+  else
+    echo "[ERROR] Fail in the run CLEAN mode"
+    exit 1
+  fi
 }
 
 # User check params
